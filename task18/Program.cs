bf422d8a2c48122a0cadc890fb810e4ff982b603
meewrage <luki_wop@mail.ru>
2023-02-21 19:45:52 +0300

    added task18

---
+++ task18/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+Console.WriteLine("Введите номер плоскости");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num == 1)
+{
+   Console.WriteLine("x > 0 and y > 0"); 
+}
+else if(num == 2)
+{
+    Console.WriteLine("x < 0 and y > 0");
+}
+else if(num == 3)
+{
+    Console.WriteLine("x < 0 and y < 0");
+}
+else if(num == 4)
+{
+    Console.WriteLine("x > 0 and y < 0");
+}
+else
+{
+Console.WriteLine("Неверно введенные данные");
+}
