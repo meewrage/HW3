fbd620cdbc124f9dbd18ef2326879ece7ab724b2
meewrage <luki_wop@mail.ru>
2023-02-21 21:21:13 +0300

    added task19

--- task19/Program.cs
+++ task19/Program.cs
@@ -3,3 +3,23 @@
 //14212 -> нет
 //12821 -> да
 //23432 -> да
+Console.WriteLine("Введите пятизначное число");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = N % 10;
+int b =(N / 10)% 10;
+int c =(N/1000)% 10;
+int d =(N/10000)% 10;
+if (9999 < N && N <= 99999)
+
+if( a == d && b== c)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+else 
+{
+    Console.WriteLine("Неверно введены данные");
+}
